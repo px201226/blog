eed6fe92352e44836f3750cf0eeb3454d5745dd8
Soo <px201226@gmail.com>
2023-08-16 15:37:31 +0900

    img 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ced4da`,
         display: `standalone`,
-        icon: `static/profile.png`,
+        icon: `static/favicon.png`,
       },
     },
     {
